11b5d0e49f4ffbe67b3ce0acd5f324b575a501a7
KangRyeon <rhrkdfus@naver.com>
2019-09-08 22:33:47 +0900

    upload NextActivity

---
+++ NextActivity/app/src/main/java/com/example/nextactivity/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.nextactivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button next_btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        next_btn = (Button)findViewById(R.id.next_btn);
+
+        // 다음화면 버튼 클릭시 - 새로운 액티비티가 틀어짐.
+        next_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, NewActivity.class);
+                startActivity(intent);
+                //finish(); // finish()
+            }
+        });
+    }
+}
